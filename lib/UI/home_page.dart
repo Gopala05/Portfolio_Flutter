d59b0f2367fff917f5568152a9cb3321d4aaa899
Gopala05 <120028677+Gopala05@users.noreply.github.com>
2023-10-06 21:05:25 +0530

    Home_Page

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -218,3 +218,51 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+  MaterialButton buildMaterialButton({required VoidCallback onTap}) {
+    return MaterialButton(
+      onPressed: onTap,
+      color: AppColors.themeColor,
+      splashColor: AppColors.lowGreen,
+      padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+      shape: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30), borderSide: BorderSide.none),
+      hoverColor: AppColors.aqua,
+      elevation: 6,
+      height: 46,
+      minWidth: 130,
+      focusElevation: 12,
+      child: Text(
+        'Download CV',
+        style: AppTextStyles.montserratStyle(color: AppColors.bgColor),
+      ),
+    );
+  }
+
+  Ink buildSocialButton({required String asset}) {
+    return Ink(
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.themeColor, width: 2.5),
+        color: AppColors.bgColor,
+        shape: BoxShape.circle,
+      ),
+      padding: const EdgeInsets.all(6),
+      child: InkWell(
+        onTap: () {},
+        radius: 50,
+        borderRadius: BorderRadius.circular(500.0),
+        hoverColor: AppColors.aqua,
+        splashColor: AppColors.lowGreen,
+        child: Image.asset(
+          asset,
+          width: 10,
+          height: 12,
+          color: AppColors.themeColor,
+          // fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
