a15c416ead113ddb3ba326fbdae234ee1c6fc833
Gopala05 <120028677+Gopala05@users.noreply.github.com>
2023-10-07 01:37:36 +0530

    App_Colors

---
+++ lib/Global/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color bgColor = const Color(0XFF003153);
+  static Color themeColor = const Color(0XFF40C4FF);
+  static Color aqua = const Color(0XFF18FFFF);
+  static Color lowGreen = const Color(0xFF7CFC00);
+  static Color bgColor2 = const Color(0XFF0277BD);
+  static Color EdgeBlue = const Color(0XFFE040FB);
+}
