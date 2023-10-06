bb44f3df1961e5562d5b9423e25feba4ed56df25
Gopala05 <120028677+Gopala05@users.noreply.github.com>
2023-10-07 01:37:22 +0530

    App_assets

---
+++ lib/Global/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static String facebook = 'assets/images/face.png';
+  static String github = 'assets/images/git.png';
+  static String linkedIn = 'assets/images/in.png';
+  static String insta = 'assets/images/insta.png';
+  static String profile1 = 'assets/images/Profile.png';
+  static String profile2 = 'assets/images/profile2.png';
+}
