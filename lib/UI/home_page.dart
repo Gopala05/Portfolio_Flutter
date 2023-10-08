ef0db91f77b459ce8ad58be67d96bebc724974e3
Gopala05 <120028677+Gopala05@users.noreply.github.com>
2023-10-08 11:46:33 +0530

    Home_page_update

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_my_portfolio/Global/AppButton.dart';
 import 'package:flutter_my_portfolio/Global/app_assets.dart';
 import 'package:flutter_my_portfolio/Global/app_colors.dart';
 import 'package:flutter_my_portfolio/Global/app_text_styles.dart';
@@ -8,11 +9,42 @@ import 'package:flutter_my_portfolio/Global/constants.dart';
 import 'package:flutter_my_portfolio/UI/about.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
-
 import '../Widgets/profile_animation.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePage createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  bool isFade = false;
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Listen to scroll events and show/hide the AboutMe() section
+    _scrollController.addListener(() {
+      if (_scrollController.offset > 80) {
+        setState(() {
+          isFade = true;
+        });
+      } else {
+        setState(() {
+          isFade = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,171 +103,160 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            // The Lottie animation widget
-            Lottie.asset(
-              'assets/images/PopUpBackground.json', // The path of the animation file
-              height: MediaQuery.of(context)
-                  .size
-                  .height, // The height of the animation
-              width:
-                  MediaQuery.of(context).size.width, // The width of the animation
-              fit: BoxFit.cover, // Fit the animation to the screen size
-              repeat: true, // Whether to repeat the animation or not
-              reverse: false, // Whether to reverse the animation or not
-              animate: true, // Whether to play the animation or not
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: size.height * 0.2,
-                  left: size.width * 0.15,
-                  right: size.width * 0.1),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
+        controller: _scrollController,
+            children: [
+              Stack(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          FadeInDown(
-                              duration: const Duration(milliseconds: 1200),
-                              child: Text(
-                                "Hello, It\'s Me",
-                                style: AppTextStyles.montserratStyle(
-                                    color: Colors.white),
-                              )),
-                          Constants.sizedBox(
-                            height: 15,
-                          ),
-                          FadeInRight(
-                              duration: const Duration(milliseconds: 1400),
-                              child: Text(
-                                "Gopala Krishna V",
-                                style: AppTextStyles.headingStyle(),
-                              )),
-                          Constants.sizedBox(
-                            height: 15,
-                          ),
-                          FadeInLeft(
-                            duration: const Duration(milliseconds: 1400),
-                            child: Row(
-                              children: [
-                                Text(
-                                  "And I am a ",
-                                  style: AppTextStyles.montserratStyle(
-                                      color: Colors.white),
-                                ),
-                                AnimatedTextKit(
-                                  animatedTexts: [
-                                    TyperAnimatedText('Android Developer',
-                                        textStyle: AppTextStyles.montserratStyle(
-                                            color: AppColors.aqua)),
-                                    TyperAnimatedText('Student',
-                                        textStyle: AppTextStyles.montserratStyle(
-                                            color: AppColors.aqua)),
-                                    TyperAnimatedText('Tech-enthusiast',
-                                        textStyle: AppTextStyles.montserratStyle(
-                                            color: AppColors.aqua)),
-                                  ],
-                                  pause: const Duration(milliseconds: 1000),
-                                  displayFullTextOnTap: true,
-                                  stopPauseOnTap: true,
-                                )
-                              ],
-                            ),
-                          ),
-                          Constants.sizedBox(
-                            height: 15,
-                          ),
-                          FadeInDown(
-                            duration: const Duration(milliseconds: 1600),
-                            child: SizedBox(
-                              width: size.width * 0.3,
-                              child: Text(
-                                'I am happy to share that this is my Portfolio '
-                                    'Web Page built using Flutter on Android Studios.',
-                                style: AppTextStyles.normalStyle(),
-                              ),
-                            ),
-                          ),
-                          Constants.sizedBox(height: 22),
-                          FadeInUp(
-                            duration: const Duration(milliseconds: 1600),
-                            child: Row(
-                              children: [
-                                buildSocialButton(asset: AppAssets.facebook),
-                                Constants.sizedBox(
-                                  width: 12,
-                                ),
-                                buildSocialButton(asset: AppAssets.github),
-                                Constants.sizedBox(
-                                  width: 12,
-                                ),
-                                buildSocialButton(asset: AppAssets.linkedIn),
-                                Constants.sizedBox(
-                                  width: 12,
-                                ),
-                                buildSocialButton(asset: AppAssets.insta)
-                              ],
-                            ),
-                          ),
-                          Constants.sizedBox(height: 22),
-                          FadeInUp(
-                            duration: const Duration(milliseconds: 1600),
-                            child: buildMaterialButton(onTap: () {}),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      const ProfileAnimation(),
-                    ],
+                  Lottie.asset(
+                    'assets/images/PopUpBackground.json', // The path of the animation file
+                    height: MediaQuery.of(context)
+                        .size
+                        .height, // The height of the animation
+                    width:
+                        MediaQuery.of(context).size.width, // The width of the animation
+                    fit: BoxFit.cover, // Fit the animation to the screen size
+                    repeat: true, // Whether to repeat the animation or not
+                    reverse: false, // Whether to reverse the animation or not
+                    animate: true, // Whether to play the animation or not
                   ),
-                  const SizedBox(
-                    height: 150,
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: size.height * 0.2,
+                        left: size.width * 0.15,
+                        right: size.width * 0.1),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                FadeInDown(
+                                    duration: const Duration(milliseconds: 500),
+                                    child: Text(
+                                      "Hello, It's Me",
+                                      style: AppTextStyles.montserratStyle(
+                                          color: Colors.white),
+                                    )),
+                                Constants.sizedBox(
+                                  height: 15,
+                                ),
+                                FadeInRight(
+                                    duration: const Duration(milliseconds: 500),
+                                    child: Text(
+                                      "Gopala Krishna V",
+                                      style: AppTextStyles.headingStyle(),
+                                    )),
+                                Constants.sizedBox(
+                                  height: 15,
+                                ),
+                                FadeInLeft(
+                                  duration: const Duration(milliseconds: 500),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "And I am a ",
+                                        style: AppTextStyles.montserratStyle(
+                                            color: Colors.white),
+                                      ),
+                                      AnimatedTextKit(
+                                        animatedTexts: [
+                                          TyperAnimatedText('Android Developer',
+                                              textStyle: AppTextStyles.montserratStyle(
+                                                  color: AppColors.aqua)),
+                                          TyperAnimatedText('Student',
+                                              textStyle: AppTextStyles.montserratStyle(
+                                                  color: AppColors.aqua)),
+                                          TyperAnimatedText('Tech-enthusiast',
+                                              textStyle: AppTextStyles.montserratStyle(
+                                                  color: AppColors.aqua)),
+                                        ],
+                                        pause: const Duration(milliseconds: 500),
+                                        displayFullTextOnTap: true,
+                                        stopPauseOnTap: true,
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Constants.sizedBox(
+                                  height: 15,
+                                ),
+                                FadeInDown(
+                                  duration: const Duration(milliseconds: 500),
+                                  child: SizedBox(
+                                    width: size.width * 0.3,
+                                    child:Text(
+                                      'I am happy to share that this is my Portfolio '
+                                          'Web Page built using Flutter on Android Studios.',
+                                      style: AppTextStyles.normalStyle(),
+                                    ),
+                                  ),
+                                ),
+                                Constants.sizedBox(height: 22),
+                                FadeInUp(
+                                  duration: const Duration(milliseconds: 500),
+                                  child: Row(
+                                    children: [
+                                      buildSocialButton(asset: AppAssets.facebook),
+                                      Constants.sizedBox(
+                                        width: 12,
+                                      ),
+                                      buildSocialButton(asset: AppAssets.github),
+                                      Constants.sizedBox(
+                                        width: 12,
+                                      ),
+                                      buildSocialButton(asset: AppAssets.linkedIn),
+                                      Constants.sizedBox(
+                                        width: 12,
+                                      ),
+                                      buildSocialButton(asset: AppAssets.insta)
+                                    ],
+                                  ),
+                                ),
+                                Constants.sizedBox(height: 22),
+                                FadeInUp(
+                                  duration: const Duration(milliseconds: 500),
+                                  child: AppButtons.buildMaterialButton(onTap: () {},
+                                  buttonName: 'Download CV'),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            const ProfileAnimation(),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 150,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: size.height * 0.75,
+                    ),
+                    child: Container(
+                      color: AppColors.bgColor2, // Set the background color to white
+                      child: Visibility(
+                        visible: isFade,
+                        child: FadeInUp(
+                          duration: const Duration(seconds: 3),
+                          child: const AboutMe(),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: size.height * 0.75,
-                ),
-              child: Container(
-                color: AppColors.bgColor2, // Set the background color to white
-                child: AboutMe(),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  MaterialButton buildMaterialButton({required VoidCallback onTap}) {
-    return MaterialButton(
-      onPressed: onTap,
-      color: AppColors.themeColor,
-      splashColor: AppColors.lowGreen,
-      padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
-      shape: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30), borderSide: BorderSide.none),
-      hoverColor: AppColors.aqua,
-      elevation: 6,
-      height: 46,
-      minWidth: 130,
-      focusElevation: 12,
-      child: Text(
-        'Download CV',
-        style: AppTextStyles.montserratStyle(color: AppColors.bgColor),
-      ),
+            ],
+          ),
     );
   }
 
