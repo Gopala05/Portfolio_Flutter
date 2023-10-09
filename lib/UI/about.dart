fd893e4a38b17652ef6b40c95bd541a8c3de316e
Gopala05 <120028677+Gopala05@users.noreply.github.com>
2023-10-09 21:09:59 +0530

    About

---
+++ lib/UI/about.dart
@@ -0,0 +1,18 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_my_portfolio/Global/app_colors.dart';
+import 'package:flutter_my_portfolio/Global/constants.dart';
+import 'package:lottie/lottie.dart';
+import '../Global/AppButton.dart';
+import '../Global/app_text_styles.dart';
+
+class AboutMe extends StatelessWidget {
+  const AboutMe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Container(
+    );
+  }
+}
