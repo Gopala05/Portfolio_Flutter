443c7cd4c9cc01c48f9288598573967425051dd7
GOPALA KRISHNA V <120028677+Gopala05@users.noreply.github.com>
2023-12-19 08:19:03 +0530

    Update app_text_styles.dart

--- lib/Global/app_text_styles.dart
+++ lib/Global/app_text_styles.dart
@@ -4,12 +4,13 @@ import 'package:google_fonts/google_fonts.dart';
 class AppTextStyles{
   static TextStyle headerTextStyle() {
     return GoogleFonts.pacifico(
-      fontSize: 20,
+      fontSize: 21,
       fontWeight: FontWeight.w500,
       color: Colors.white,
       letterSpacing: 1
     );
   }
+  
   static TextStyle montserratStyle({required Color color}){
     return GoogleFonts.alegreya(
       color: color,
@@ -18,6 +19,7 @@ class AppTextStyles{
       letterSpacing: 2
     );
   }
+  
   static TextStyle headingStyle({ Color color = Colors.white,}){
     return GoogleFonts.abrilFatface(
       fontSize: 60,
@@ -26,6 +28,7 @@ class AppTextStyles{
       letterSpacing: 3,
     );
   }
+  
   static TextStyle normalStyle(){
     return GoogleFonts.signikaNegative(
       fontWeight: FontWeight.w500,
