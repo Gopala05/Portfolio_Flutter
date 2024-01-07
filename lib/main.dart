c009e78a30f2f54870aef026ae740dc02feb5df5
GOPALA KRISHNA V <120028677+Gopala05@users.noreply.github.com>
2024-01-07 19:49:39 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Portfolio',
+      title: 'Portfolio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
